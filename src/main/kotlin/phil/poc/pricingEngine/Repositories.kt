9b66d03aa4d4c769bd1b3122d4ba2579f8cce9ca
PhilOfCalth <philiplarkin8383@gmail.com>
2020-02-09 19:46:21 +0000

    Simplistic pagnation

--- src/main/kotlin/phil/poc/pricingEngine/Repositories.kt
+++ src/main/kotlin/phil/poc/pricingEngine/Repositories.kt
@@ -1,9 +1,9 @@
 package phil.poc.pricingEngine.repositories
 
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import phil.poc.pricingEngine.models.Product
 
 @RepositoryRestResource(excerptProjection = Product::class)
-interface ProductRepository: CrudRepository<Product, Long> {
+interface ProductRepository: PagingAndSortingRepository<Product, Long> {
 }
